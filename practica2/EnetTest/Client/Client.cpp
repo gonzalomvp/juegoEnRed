ce2b11d134f4ba75b8e1a74dddba5de5db8c68ab
gonzalomvp <gonzalomvp@hotmail.com>
2018-04-25 20:57:44 +0200

    practica 2 initial version

---
+++ practica2/EnetTest/Client/Client.cpp
@@ -0,0 +1,50 @@
+#include "common/stdafx.h"
+#include "ClientENet.h"
+#include <Windows.h>
+
+using namespace ENet;
+
+int Main(void)
+{
+	CORE_InitSound();
+	FONT_Init();
+	//
+	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT); // Sets up clipping
+	glClearColor(0.0f, 0.1f, 0.3f, 0.0f);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, SCR_WIDTH, 0.0, SCR_HEIGHT, 0.0, 1.0);
+	glEnable(GL_TEXTURE_2D);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	GLuint texture = CORE_LoadPNG("data/ball.png", false);
+
+    CClienteENet* pClient = new CClienteENet();
+    pClient->Init();
+
+    CPeerENet* pPeer = pClient->Connect("127.0.0.1", 1234, 2);
+
+    std::vector<CPacketENet*>  incommingPackets;
+    pClient->Service(incommingPackets, 0);
+    Sleep(100);
+    pClient->SendData(pPeer, "pepe", 4, 0, false);
+    while (true)
+    {
+		glClear(GL_COLOR_BUFFER_BIT);
+		CORE_RenderCenteredSprite(vmake(200, 200), vmake(32, 32), texture, 1.0f);
+        std::vector<CPacketENet*>  incommingPackets;
+        pClient->Service(incommingPackets, 0);
+        Sleep(100);
+
+		SYS_Show();
+		SYS_Pump();
+		SYS_Sleep(17);
+    }
+
+    pClient->Disconnect(pPeer);
+
+	FONT_End();
+	CORE_EndSound();
+    return 0;
+}
+
